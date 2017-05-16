d1a5d2f95576ebdb6303f8fcfa30c1d965b58f94
ixialuo <ixialuo@gmail.com>
2017-05-16 16:54:51 +0800

    first commit

--- ICropViewController/ViewController.swift
+++ ICropViewController/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import CTAssetsPickerController
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var height: NSLayoutConstraint!
+    @IBOutlet weak var width: NSLayoutConstraint!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    @IBAction func selectedPhotoBtnAction(_ sender: UIButton) {
+        
+        IImagePickerManger.shared.selectedStyle = .multiple
+        IImagePickerManger.shared.editingStyle = .custom
+        IImagePickerManger.shared.delegate = self
+        IImagePickerManger.shared.showImageActionSheet(invoker: self)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -22,4 +37,20 @@ class ViewController: UIViewController {
 
 
 }
+
+extension ViewController: IImagePickerMangerDelegate {
+    func cropViewController(picker: UIViewController, images: [UIImage], thumImages: [UIImage]?) {
+        picker.dismiss(animated: true, completion: nil)
+        //imageView.image = images[0]
+        width.constant = images[0].size.width
+        height.constant = images[0].size.height
+        
+        //UIImageWriteToSavedPhotosAlbum(images[0], self, #selector(image(image:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    func image(image: UIImage, didFinishSavingWithError error: NSError?, contextInfo:UnsafeRawPointer){
+    }
+}
+
+
 
---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, ‘8.0’
+
+target 'ICropViewController' do
+  use_frameworks!
+  
+  pod 'CTAssetsPickerController'
+  pod 'TOCropViewController'
+
+end
